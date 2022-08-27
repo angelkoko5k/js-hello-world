e4f6ee0aab7c5c5ba132f69670ef4bdc4a4558f3
angelkoko5k <angelkoko5k@gmail.com>
2022-08-27 08:28:58 +0000

    fix

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  return res.send('<h4>wellcom to Nako-meo to heroku</h4>')
+  return res.send('<body style="background-color: #92a8d1"></body> <h1 >ลูงตู่20ปี</h1>')
 })
 
 const PORT = process.env.PORT || 3000
