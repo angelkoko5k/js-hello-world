5cb87b8b06fa5256a1765f85d1eff95618bf1cc2
Monthira19 <poo19052545@hotmail.com>
2022-08-28 08:01:16 +0000

    test

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  return res.send('<body style="  background-color: #CC99CC" ></body> <ul><li><a href="default.asp">หน้าหลัก</a></li><li><a href="news.asp">ข่าว</a></li><li><a href="contact.asp">เนื้อหา</a></li><li><a href="about.asp">เกี่ยวกับ</a></li></ul>')
+  return res.send('<body style="  background-color: #ffafb0" ></body> <ul><li><a href="default.asp">หน้าหลัก</a></li><li><a href="news.asp">ข่าว</a></li><li><a href="contact.asp">เนื้อหา</a></li><li><a href="about.asp">เกี่ยวกับ</a></li></ul>')
 })
 
 const PORT = process.env.PORT || 8000
