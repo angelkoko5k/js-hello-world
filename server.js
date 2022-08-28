716588e7f863774c40317f0390c04cd2eca99cb0
angelkoko5k <angelkoko5k@gmail.com>
2022-08-28 04:49:32 +0000

    fix

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  return res.send('<body style="background-color: #d5f4e6" ></body> <ul><li><a href="default.asp">หน้าหลักddddd.</a></li><li><a href="news.asp">ข่าว</a></li><li><a href="contact.asp">เนื้อหา</a></li><li><a href="about.asp">เกี่ยวกับ</a></li></ul>')
+  return res.send('<body style="  background-color: #d5f4e6" ></body> <ul><li><a href="default.asp">หน้าหลักddddd.</a></li><li><a href="news.asp">ข่าว</a></li><li><a href="contact.asp">เนื้อหา</a></li><li><a href="about.asp">เกี่ยวกับ</a></li></ul>')
 })
 
 const PORT = process.env.PORT || 8000
