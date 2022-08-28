bce03c5433efe6f3d042bdedbfac73d0db607d1d
Amonrat13 <aay.thee1309@gmail.com>
2022-08-28 08:43:44 +0000

    fixxx

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  return res.send('<body style="  background-color: #FF69B4" ></body> <ul><li><a href="default.asp">หน้าเมนูหลัก</a></li><li><a href="news.asp">ข่าวสาร</a></li><li><a href="contact.asp">เนื้อหา</a></li><li><a href="about.asp">เกี่ยวกับเว็บไซต์</a></li></ul>')
+  return res.send('<body style="  background-color: #FF69B4" ></body> <ul><li><a href="default.asp">หน้าเมนูหลัก</a></li><li><a href="news.asp">ข่าวสาร</a></li><li><a href="contact.asp">เนื้อหาเว็บไซต์</a></li><li><a href="about.asp">เกี่ยวกับเว็บไซต์</a></li></ul>')
 })
 
 const PORT = process.env.PORT || 8000
